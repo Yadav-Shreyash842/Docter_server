9629446a28cad4c76bb0d6137981a8564830b201
Yadav Shreyash <yadavshreyash848@gmail.com>
2026-05-05 14:08:52 +0530

    Update CORS to allow Vercel frontend

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://docter-client-orpin.vercel.app',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
